cfe9ce4afb0fcfee6e6078af19a372befb47cabe
sirdraku <83011440+sirdraku@users.noreply.github.com>
2021-06-10 13:50:20 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
           },
         },
         // url: process.env.WPGRAPHQL_URL,
-        url: `https://web.amanuntul.ro/graphql`,
+        url: `https://www.luxuryandglamor.com/graphql`,
         verbose: true,
         develop: {
           hardCacheMediaFiles: false,
